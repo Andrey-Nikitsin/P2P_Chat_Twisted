be256d52e00a1056caa8666abb2caad055a6d5be
Andrey-Nikitsin <the.niki.andrey@gmail.com>
2021-12-31 17:37:42 +0300

    P2P server

--- P2P_server.py
+++ P2P_server.py
@@ -4,55 +4,66 @@ from twisted.internet.protocol import ServerFactory
 
 
 
+
 class P2PChat(LineReceiver):
     def __init__(self, users):
         self.users = users
+        self.name_user = False
+        self.SelectName = 0
 
     def connectionMade(self):
+        print('connect')
         self.transport.write(b'SERVER CONNECT\n')
         self.transport.write(b'What you name?\n')
         self.users.update({'':self})
 
-    shet =0
-    def lineReceived(self, line):
+    def dataReceived(self, data):
+
+        def SelectUser():
+            self.transport.write(b'select user:\n')
+            self.transport.write(' '.join(self.users.keys()).encode("utf-8"))
+            self.transport.write(b'\n')
+
+        def UserVerification(line):
+            name = line.decode("utf-8")
+            if name[:-2] in self.users.keys():
+                Name = self.users.get(name[:-2])
+                self.SelectName = Name
+            else:
+                self.transport.write(b'User is not found\n')
+
+        def SendMessange(data):
+            name = self.SelectName
+            for user in self.users.keys():
+                if self.users.get(user) == self:
+                    user = user.encode("UTF-8")
+                    name.transport.write(b'messenge from:')
+                    name.transport.write(user)
+                    name.transport.write(b'  ')
+                    name.transport.write(data)
 
         def WriteNameUser(data):
             data = data.decode("UTF-8")
             if len(self.users) >0:
                 if data in self.users.keys():
-                    self.transport.write(b'NAME IS TAKEN\n')
+                    self.transport.write(b'NAME IS TAKEN, ENTER AGAIN \n')
                 else:
                     self.users.pop('')
-                    self.users.update({data:self})
+                    self.users.update({data[:-2]:self})
+                    self.name_user = True
             else:
-                self.users.update({data: self})
+                self.users.update({data[:-2]: self})
+                self.name_user= True
 
-        if '' in self.users.keys():
-            WriteNameUser(line)
-
-        self.transport.write(b'select user\n')
-        self.transport.write(' '.join(self.users.keys()).encode("utf-8"))
-
-
-        def UserVerification(line):
-            name = line.decode("utf-8")
-            if name in self.users.keys():
-                Name = self.users.get(name)
-                return Name
+        if self.name_user == False:
+            WriteNameUser(data)
+            if self.name_user == True:
+                SelectUser()
+        else:
+            if self.SelectName == 0:
+                UserVerification(data)
             else:
-                self.transport.write(b'User is not found')
-                return 1
-
-        def SendMessange(name):
-            name.transport.write(b'hi')
-
-        if self.shet > 0:
-            if UserVerification(line) !=1:
-                SendMessange(UserVerification(line))
-
-        self.shet += 1
-
-
+                SendMessange(data)
 
 
 class P2PChatFactory(ServerFactory):
